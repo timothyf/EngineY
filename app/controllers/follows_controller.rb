9c7828a2a48bc31753d55fdfdde67a8f7844d1eb
timothyf <timothyf@gmail.com>
2010-03-06 00:10:28 -0500

    added json to follows controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,6 +5,7 @@ class FollowsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @follows }
+      format.json { render :json => @follows }
     end
   end
 
@@ -14,6 +15,7 @@ class FollowsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @follow }
+      format.json { render :json => @follow }
     end
   end
 
@@ -35,9 +37,11 @@ class FollowsController < ApplicationController
         flash[:notice] = 'Follow was successfully created.'
         format.html { redirect_to(@follow) }
         format.xml  { render :xml => @follow, :status => :created, :location => @follow }
+        format.json { render :json => @follow, :status => :created, :location => @follow }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @follow.errors, :status => :unprocessable_entity }
+        format.json { render :json => @follow.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -50,9 +54,11 @@ class FollowsController < ApplicationController
         flash[:notice] = 'Follow was successfully updated.'
         format.html { redirect_to(@follow) }
         format.xml  { head :ok }
+        format.json { head :ok }
       else
         format.html { render :action => "edit" }
         format.xml  { render :xml => @follow.errors, :status => :unprocessable_entity }
+        format.json { render :json => @follow.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -64,6 +70,7 @@ class FollowsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to(follows_url) }
       format.xml  { head :ok }
+      format.json { head :ok }
     end
   end
 end
