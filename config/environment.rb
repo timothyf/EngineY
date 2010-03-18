6e3c2d7ff9e49d717beab24169666a4ae5aac3c7
timothyf <timothyf@gmail.com>
2010-03-17 22:30:33 -0400

    theme check fixes

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
 # Be sure to restart your server when you modify this file
+#ENV['GEM_PATH'] = File.expand_path('~/.gems') + ':/usr/lib/ruby/gems/1.8'
 
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
+#ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
