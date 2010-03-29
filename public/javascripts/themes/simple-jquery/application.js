9fa3419db6a438a3229a2aef77b93c6b2ddc8aa3
timothyf <timothyf@gmail.com>
2010-03-28 23:29:29 -0400

    finished simple-jquery theme

--- public/javascripts/themes/simple-jquery/application.js
+++ public/javascripts/themes/simple-jquery/application.js
@@ -17,27 +17,19 @@ function site_init() {
 
 function submit_wall_post() {
 	before_wall_post();
-	dojo.xhrPost({
-	    form: "wall_post_entry_form",
-	    timeout: 3000, // give up after 3 seconds
-	    content: { authenticity_token:authenticity_token },
-		load: function (data) {wall_post_saved(data);}
-	});
+	$.post("/wall_posts/create", 
+		   $("#wall_post_entry_form").serialize(),
+		   function (data) {wall_post_saved(data);});
 }
 
 function delete_wall_post(id) {
-	dojo.xhrPost({
-		url: '/wall_posts/delete',
-	    timeout: 3000, // give up after 3 seconds
-	    content: { id:id, authenticity_token:authenticity_token },
-		load: function (data) {wall_post_deleted(data);}
-	});	
+	$.post('/wall_posts/delete',
+	       { id:id },
+		    function (data) {wall_post_deleted(data);});
 }
 
 function wall_post_deleted(request) {
-	$('wall_posts_content').innerHTML = request;
-	// clear wall post message area, need to clear tinymce i think
-	$('wall_post_message').value == '';
+	$("#wall_posts_content").html(request);
 }
 
 function before_wall_post() {
@@ -48,11 +40,11 @@ function before_wall_post() {
 // have to call this after the new tinymce editor is loaded upon a save
 // tinyMCE.execCommand('mceAddControl', false, id); 
 
-
 function wall_post_saved(request) {
-	$('wall_posts_content').innerHTML = request;
-	// clear wall post message area, need to clear tinymce i think
-	$('wall_post_message').value == '';
+	$("#wall_posts_content").html(request);
+	// clear wall post message area
+	//$("#wall_post_message").val('');
+	tinyMCE.getInstanceById('wall_post_message').setContent('');
 }
 
 function show_wall_post_entry() {
@@ -61,26 +53,17 @@ function show_wall_post_entry() {
 
 function cancel_wall_post() {
 	$("#wall_post_entry").slideUp();
+	tinyMCE.getInstanceById('wall_post_message').setContent('');
 }
-
-/*
-$(document).ajaxSend(function(event, request, settings) {
-  if (typeof(AUTH_TOKEN) == "undefined") return;
-  // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
-  settings.data = settings.data || "";
-  settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
-}); */
 
 function handle_promote_to_admin_btn() {
 	var rows = dijit.byId('gridNode').selection.getSelected();
 	for (i=0; i<rows.length; i++) {
 		// send ajax request to promote user to group admin
-		dojo.xhrPost({
-			url: '/memberships/update',
-		    timeout: 3000, // give up after 3 seconds
-		    content: { id:rows[i].id, admin:true, authenticity_token:authenticity_token },
-			load: function (data) {member_promoted(data);}
-		});	
+		$.post('/memberships/update',
+			   { id:rows[i].id, admin:true },
+			   function (data) {member_promoted(data);}
+			);
 	}
 }
 
@@ -92,17 +75,15 @@ function handle_demote_from_admin_btn() {
 	var rows = dijit.byId('gridNode').selection.getSelected();
 	for (i=0; i<rows.length; i++) {
 		// send ajax request to demote user from group admin
-		dojo.xhrPost({
-			url: '/memberships/update',
-		    timeout: 3000, // give up after 3 seconds
-		    content: { id:rows[i].id, admin:false, authenticity_token:authenticity_token },
-			load: function (data) {member_demoted(data);}
-		});	
+		$.post('/memberships/update',
+			   { id:rows[i].id, admin:false },
+			   function (data) {member_demoted(data);}
+			);
 	}
 }
 
-function member_promoted(data) {
-	alert('member promoted = ' + data);
+function member_demoted(data) {
+	alert('member demoted = ' + data);
 }
 
 function handle_remove_from_group_btn() {
@@ -146,34 +127,22 @@ function display_blog_settings() {
 }
 
 function show_blog_drafts() {
-	var drafts = dojo.query(".blog_post_draft");
-	if (drafts.length > 0 && (drafts[0].style.display == 'none' || drafts[0].style.display == '')) {
-		// select all elements with class 'blog_post_draft' and change display
-		// property to 'block'
-		for (i=0; i<drafts.length; i++) {
-			drafts[i].style.display = 'block';
-		}
-		$('show_drafts_btn').innerHTML = 'Hide Drafts';
+	if ($('#show_drafts_btn').html() == 'Show Drafts') {
+		$(".blog_post_draft").css("display","block");
+		$('#show_drafts_btn').html('Hide Drafts');
 	}
-	else if (drafts.length > 0) {
-		// select all elements with class 'blog_post_draft' and change display
-		// property to 'hide'
-		for (i=0; i<drafts.length; i++) {
-			drafts[i].style.display = 'none';
-		}
-		$('show_drafts_btn').innerHTML = 'Show Drafts';
+	else {
+		$(".blog_post_draft").css("display","none");
+		$('#show_drafts_btn').html('Show Drafts');	
 	}
 }
 
 function post_status(event) {
-	activity_layout_id = dojo.byId('status_post_entry_form').activity_layout_id.value;
-	layout_id = dojo.byId('status_post_entry_form').layout_id.value;
-	dojo.xhrPost({
-	    form: "status_post_entry_form",
-	    timeout: 3000, // give up after 3 seconds
-	    content: { authenticity_token:authenticity_token },
-		load: function (data) {status_post_saved(data, layout_id, activity_layout_id);}
-	});
+	activity_layout_id = $("#activity_layout_id").val();
+	layout_id = $("#layout_id").val();
+	$.post('/status_posts/create',
+	       $("#status_post_entry_form").serialize(),
+		   function (data) {status_post_saved(data, layout_id, activity_layout_id);});
 }
 
 // Called after new status successfully posted to server
@@ -187,8 +156,8 @@ function status_post_saved(data, layout_id, activity_layout_id) {
  * Called when the Only Statuses checkbox is checked in the activity stream of a users profile page.
  */
 function filter_activities(layout_id) {
-	var status = dojo.byId('status_checkbox').checked;
-	var include_friends = dojo.byId('friends_checkbox').checked;
+	var status = $('#status_checkbox').attr('checked');
+	var include_friends = $('#friends_checkbox').attr('checked');
 	
 	var only_statuses = (status == true ? 'true':'false')
 	var include_friends = (include_friends == true ? 'true':'false')
@@ -202,8 +171,8 @@ function filter_activities(layout_id) {
  * Called when the Only Friends checkbox is checked in the activity stream on the home page.
  */
 function only_friends_activities(layout_id) {
-	var include_friends = dojo.byId('friends_checkbox').checked;	
-	var include_friends = (include_friends == true ? 'true':'false')	
+	var include_friends = $('#friends_checkbox').attr('checked');	
+	include_friends = (include_friends == true ? 'true':'false')	
 	var layout = find_layout_by_id(layout_id);
 	layout.properties = {include_friends:include_friends};
 	layout.load();
@@ -219,44 +188,35 @@ function message_delete() {
 
 function add_blog_post_comment() {
 	//tinyMCE.triggerSave(true,true);
-	dojo.xhrPost({
-	    form: "comment_entry_form",
-	    timeout: 3000, // give up after 3 seconds
-	    content: { authenticity_token:authenticity_token },
-		load: function (data) {blog_post_comment_saved(data);}
-	});
+	$.post('/blog_posts/add_comment',
+		   $("#comment_entry_form").serialize(),
+		   function (data) {blog_post_comment_saved(data);});
 }
 
 function blog_post_comment_saved(request) {
-	$('blog_post_comments_area').innerHTML = request;
+	$('#blog_post_comments_area').html(request);
 }
 
 function add_photo_comment() {
 	//tinyMCE.triggerSave(true,true);
-	dojo.xhrPost({
-	    form: "comment_entry_form",
-	    timeout: 3000, // give up after 3 seconds
-	    content: { authenticity_token:authenticity_token },
-		load: function (data) {photo_comment_saved(data);}
-	});
+	$.post('/photos/add_comment',
+		   $("#comment_entry_form").serialize(),
+		   function (data) {photo_comment_saved(data);});
 }
 
 function photo_comment_saved(request) {
-	$('photo_comments_area').innerHTML = request;
+	$('#photo_comments_area').html(request);
 }
 
 function submit_forum_post_reply() {
 	tinyMCE.triggerSave(true,true);
-	dojo.xhrPost({
-	    form: "forum_post_reply_form",
-	    timeout: 3000, // give up after 3 seconds
-	    content: { authenticity_token:authenticity_token },
-		load: function (data) {forum_post_reply_saved(data);}
-	});	
+	$.post($("#forum_post_reply_path").val(),
+		   $("#forum_post_reply_form").serialize(),
+		   function (data) {forum_post_reply_saved(data);});
 }
 
 function forum_post_reply_saved(request) {
-	$('existing_replies').innerHTML = request;
+	$('#existing_replies').html(request);
 	tinyMCE.activeEditor.setContent("");
 }
 
